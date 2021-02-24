a4fc4138e04992a24abe0506b9a7aca3c57fb29d
dzyuan <dzyuan8@126.com>
2021-02-24 17:10:15 +0800

    修改checkauth

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
@@ -32,10 +32,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET,POST,PATCH,DELETE,OPTIONS,PUT"
   );
-  res.setHeader(
-    "Access-Control-Allow-Credentials",
-    "true"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Credentials",
+  //   "true"
+  // );
   next();
 });
 
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,11 +3,11 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         // if (!req.headers.authorization)
-
+        let token="";
         //  return;
         console.log(req.headers);
         if (req.headers.authorization) {
-            const token = req.headers.authorization.split(" ")[1];
+            token = req.headers.authorization.split(" ")[1];
         }
         const decodedToken = jwt.verify(token, "long long ago");
         req.userData = { username: decodedToken.username, userId: decodedToken.userId };
